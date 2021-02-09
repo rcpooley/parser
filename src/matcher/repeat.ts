6d4e30d547a7a66112da2848e6c7584650f17e03
Robert Pooley <robertcpooley@gmail.com>
2021-02-08 22:47:10 -0800

    Cleaned up repeat logic

--- src/matcher/repeat.ts
+++ src/matcher/repeat.ts
@@ -1,32 +1,36 @@
 import Match from '.';
 import { RepeatType } from '../schema';
-import { PartialGroupMatcher } from './group';
 import { Matcher, MatcherAndSections, Params, State } from './matcher';
 
 class PartialRepeatMatcher extends Matcher {
     private matcher: MatcherAndSections;
     private remainingMatcher: PartialRepeatMatcher | null;
+    private nextMatchSeparator: boolean;
     length: number;
+    separatorAtEnd: boolean;
 
     constructor(
         params: Params,
         state: State,
-        private repeatID: number,
-        private separatorID: number | null
+        private type: RepeatType,
+        private matchSeparator: boolean
     ) {
         super(params, state);
         let matcher: Matcher;
-        if (separatorID !== null) {
-            matcher = new PartialGroupMatcher(this.getParams(-1), state, [
-                repeatID,
-                separatorID,
-            ]);
+        if (matchSeparator) {
+            if (type.separator === null) {
+                throw new Error('not possible');
+            }
+            matcher = Match(this.getParams(type.separator.id), state);
+            this.nextMatchSeparator = false;
         } else {
-            matcher = Match(this.getParams(repeatID), state);
+            matcher = Match(this.getParams(type.childID), state);
+            this.nextMatchSeparator = type.separator !== null;
         }
         this.matcher = new MatcherAndSections(matcher);
         this.remainingMatcher = null;
         this.length = 0;
+        this.separatorAtEnd = false;
     }
 
     protected nextImpl(): State | null {
@@ -34,6 +38,7 @@ class PartialRepeatMatcher extends Matcher {
         // Make sure we matched at least one child (to fix repeat of optional infinite)
         if (sections === null || sections[this.state.index].length === 0) {
             this.length = 0;
+            this.separatorAtEnd = false;
             return null;
         }
         if (this.remainingMatcher === null) {
@@ -41,16 +46,23 @@ class PartialRepeatMatcher extends Matcher {
                 this.params,
                 {
                     sections,
-                    index:
-                        this.state.index + (this.separatorID === null ? 1 : 2),
+                    index: this.state.index + 1,
                 },
-                this.repeatID,
-                this.separatorID
+                this.type,
+                this.nextMatchSeparator
             );
             if (!this.remainingMatcher.hasNext()) {
                 this.remainingMatcher = null;
                 this.matcher.next();
+                if (
+                    this.type.separator !== null &&
+                    this.type.separator.end !== 'optional' &&
+                    this.type.separator.end !== this.matchSeparator
+                ) {
+                    return this.nextImpl();
+                }
                 this.length = 1;
+                this.separatorAtEnd = this.matchSeparator;
                 return { sections, index: this.state.index };
             }
         }
@@ -61,6 +73,7 @@ class PartialRepeatMatcher extends Matcher {
         }
         sections = this.remainingMatcher.next().sections;
         this.length = this.remainingMatcher.length + 1;
+        this.separatorAtEnd = this.remainingMatcher.separatorAtEnd;
         return {
             sections,
             index: this.state.index,
@@ -70,96 +83,32 @@ class PartialRepeatMatcher extends Matcher {
 
 export default class RepeatMatcher extends Matcher {
     private matcher: PartialRepeatMatcher;
-    private endMatcher: Matcher | null;
     private returnedAny: boolean;
-    private returnedEmpty: boolean;
 
     constructor(params: Params, state: State, private type: RepeatType) {
         super(params, state);
-        this.matcher = new PartialRepeatMatcher(
-            params,
-            state,
-            type.childID,
-            type.separator?.id || null
-        );
-        this.endMatcher = null;
+        this.matcher = new PartialRepeatMatcher(params, state, type, false);
         this.returnedAny = false;
-        this.returnedEmpty = false;
     }
 
     protected nextImpl(): State | null {
-        const check = this.endMatcherNext();
-        if (check !== null) {
-            return check;
-        }
         if (!this.matcher.hasNext()) {
-            if (
-                this.returnedAny ||
-                this.returnedEmpty ||
-                this.type.minElementCount > 0
-            ) {
+            if (this.returnedAny || this.type.minElementCount > 0) {
                 return null;
             }
-            this.returnedEmpty = true;
+            this.returnedAny = true;
             return this.groupChildren(this.state.sections, 0, 'repeat');
         }
-        const sections = this.matcher.next().sections;
         // Guaranteed that matcher.length > 0
-        if (this.matcher.length < this.type.minElementCount) {
+        const sections = this.matcher.next().sections;
+        let numElements = this.matcher.length;
+        if (this.type.separator !== null) {
+            numElements = Math.floor((numElements + 1) / 2);
+        }
+        if (numElements < this.type.minElementCount) {
             return this.nextImpl();
         }
-
-        if (this.type.separator === null) {
-            this.returnedAny = true;
-            return this.groupChildren(sections, this.matcher.length, 'repeat');
-        } else if (this.type.separator.end === true) {
-            // Separator required at end so don't need to match another child
-            this.returnedAny = true;
-            return this.groupChildren(
-                sections,
-                this.matcher.length * 2,
-                'repeat'
-            );
-        } else {
-            // Separator optional or required not at end so try to match another child
-            this.endMatcher = Match(this.getParams(this.type.childID), {
-                sections,
-                index: this.state.index + this.matcher.length * 2,
-            });
-            const check = this.endMatcherNext();
-            if (check !== null) {
-                // Child found, safe to return for both options
-                this.returnedAny = true;
-                return check;
-            }
-            if (this.type.separator.end === 'optional') {
-                this.returnedAny = true;
-                return this.groupChildren(
-                    sections,
-                    this.matcher.length * 2,
-                    'repeat'
-                );
-            } else {
-                // Child not found, remove last separator from repeat
-                this.returnedAny = true;
-                return this.groupChildren(
-                    sections,
-                    this.matcher.length * 2 - 1,
-                    'repeat'
-                );
-            }
-        }
-    }
-
-    private endMatcherNext(): State | null {
-        if (this.endMatcher !== null && this.endMatcher.hasNext()) {
-            return this.groupChildren(
-                this.endMatcher.next().sections,
-                this.matcher.length * 2 + 1,
-                'repeat'
-            );
-        }
-        this.endMatcher = null;
-        return null;
+        this.returnedAny = true;
+        return this.groupChildren(sections, this.matcher.length, 'repeat');
     }
 }
